2c0059d1135549151df4a2f1d862e694cc47fb4b
Damien Merian <qashdzn@gmail.com>
2024-09-25 20:17:30 +0200

    rails update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(name: "Epicure", address: "75008 Paris", phone_number: "123456789", category: "french")
+Restaurant.create!(name: "Sushi Shop", address: "75009 Paris", phone_number: "987654321", category: "japanese")
+Restaurant.create!(name: "Pizza Hut", address: "75010 Paris", phone_number: "543216789", category: "italian")
+Restaurant.create!(name: "Chinatown", address: "75011 Paris", phone_number: "654321789", category: "chinese")
+Restaurant.create!(name: "Frites Maison", address: "75012 Paris", phone_number: "098765432", category: "belgian")
